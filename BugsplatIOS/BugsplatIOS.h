89d715f8e2dbf434dd390f58ff7495c4b341f08e
Geoff Raeder <geoff@truqcapp.com>
2021-05-26 07:12:16 -0400

    Initial Commit

---
+++ BugsplatIOS/BugsplatIOS.h
@@ -0,0 +1,18 @@
+//
+//  BugsplatIOS.h
+//  BugsplatIOS
+//
+//  Created by Geoff Raeder on 5/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BugsplatIOS.
+FOUNDATION_EXPORT double BugsplatIOSVersionNumber;
+
+//! Project version string for BugsplatIOS.
+FOUNDATION_EXPORT const unsigned char BugsplatIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugsplatIOS/PublicHeader.h>
+
+
